6ceaf15fe37d0003fa968df505806ffb52bc5aaa
Michael Espiritu <74516247+michaelEspir-itu@users.noreply.github.com>
2020-11-18 18:54:14 -0800

    first week of assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+var appetizerPrice = 10.50;
+var mainCoursePrice = 25;
+
+var levisWallet = 50;
+var coinPurse = 10;
+
+var numberOfbestFriends = 2;
+var acquitancenumberOfFriends = 3;
+
+var bestFriend = 40;
+var acquitance = 25;
+
+var bestFriendsName = "Benjamin";
+var acquitanceName  = "Enrique";
+
+var bestFriendlastName = " Go";
+var acquitancelastName = " Diaz";
+
+var bestFriendMiddleInitial = " Ho";
+var acquitanceMiddleInitial = " Cano";
+
+levisWallet = levisWallet - mainCoursePrice;
+
+console.log('my new balance is' + levisWallet);
+
+bestFriend = bestFriend / numberOfbestFriends;
+
+console.log('my new friends are '  + bestFriend);
+
+var fullName = bestFriendsName + ' ' + bestFriendMiddleInitial + ' ' + bestFriendlastName; 
+console.log(' my best friend full name '+ fullName);
